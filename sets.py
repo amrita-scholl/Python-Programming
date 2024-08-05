2fa552d1a99a5ec7bdb616ed1ca80c4482768afb
Amrita Singh <amrita.scholl@gmail.com>
2024-08-05 17:30:26 +0530

    Sets in Python

--- sets.py
+++ sets.py
@@ -9,13 +9,24 @@ my_list2 = [3, 0, 2, 9]
 # Create a set from my_list1 for O(1) lookups
 set1 = set(my_list1)
 set2 = set(my_list2)
+set3 = set1.copy()
 
-union_set = set1.union(set2)
-intersection_set = set1.intersection(set2)
-difference_set = set2.difference(set1)
-sym_diff_set = set2.symmetric_difference(set1)
+set1.add(2)
+#set1.clear()
+
+
+union_set = set3.union(set2)
+intersection_set = set3.intersection(set2)
+set2.intersection_update(set3)
+difference_set = set1.difference(set2)
+#difference_update = set1.difference_update(set2)
+sym_diff_set = set2.symmetric_difference(set3)
+
+set1.discard(9)
 
 print(union_set)
 print(intersection_set)
 print(difference_set)
+print(set1)
+print(set2)
 print(sym_diff_set)
