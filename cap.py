b455eca8e9e3df202ab5831b956dbb8472194412
Amrita Singh <Amrita.Singh@ad.creospan.com>
2025-03-03 14:01:33 +0530

    Adding More Practice files

---
+++ cap.py
@@ -0,0 +1,6 @@
+def cap_text(text):
+    '''
+    Input String
+    Output the Capitalized String
+    '''
+    return text.title()
---
+++ simple.py
@@ -0,0 +1,14 @@
+'''
+A Simple Function 
+'''
+
+def my_func():
+    '''
+    A Simple Function 
+    '''
+    a=5
+    b=7
+    print(a)
+    print(b)
+
+my_func()
